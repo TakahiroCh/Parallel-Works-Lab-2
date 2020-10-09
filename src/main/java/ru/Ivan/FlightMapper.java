ae98068bec997c0061dd191536c81f1b6ddc6fba
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 00:02:10 +0300

    Scripted auto-commit on change (2020-10-10 00:02:10) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightMapper.java
+++ src/main/java/ru/Ivan/FlightMapper.java
@@ -14,6 +14,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableCompa
     public void main(LongWritable key, Text value, Context context) throws IOException {
         String[] table = value.toString().split(DELIMITER);
         int aeroportID = Integer.parseInt(table[14]);
+        int 
 
     }
 }
