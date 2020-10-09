0fea87fb6d6649c9d1fad207643723c65200eb8e
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 01:44:31 +0300

    Scripted auto-commit on change (2020-10-10 01:44:31) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightReduce.java
+++ src/main/java/ru/Ivan/FlightReduce.java
@@ -4,13 +4,18 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class FlightReduce extends Reducer<FlightWritableComparable, Text, Text, Text> {
 
     @Override
     protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
-        
+        Iterator<Text> iter = values.iterator();
+        Text nameInfo = new Text(iter.next());
+        while(iter.hasNext()) {
+            
+        }
 
     }
 
