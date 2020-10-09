e8ac50b8ce255b6a03e8bb9ceb4383d6a78795ae
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-09 23:30:01 +0300

    Scripted auto-commit on change (2020-10-09 23:30:01) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightWritableComparable.java
+++ src/main/java/ru/Ivan/FlightWritableComparable.java
@@ -2,7 +2,34 @@ package ru.Ivan;
 
 import org.apache.hadoop.io.WritableComparable;
 
-public class FlightWritableComparable implements WritableComparable {
-    
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
+public class FlightWritableComparable implements WritableComparable {
+
+    private int aeroportID;
+    private int indenity;
+
+    public FlightWritableComparable() {}
+
+    public FlightWritableComparable(int aeroportID, int indenity) {
+        this.aeroportID = aeroportID;
+        this.indenity = indenity;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
