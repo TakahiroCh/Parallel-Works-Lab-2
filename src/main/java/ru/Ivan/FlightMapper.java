944e99016a68ff106c6bfad2ef917a0e71c1b659
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-09 23:36:59 +0300

    Scripted auto-commit on change (2020-10-09 23:36:59) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightMapper.java
+++ src/main/java/ru/Ivan/FlightMapper.java
@@ -7,5 +7,5 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, FlightWritable> {
 
     @Override
-    public void main
+    public void main(LongWritable key, Text value, FlightWritableComparable output,)
 }
