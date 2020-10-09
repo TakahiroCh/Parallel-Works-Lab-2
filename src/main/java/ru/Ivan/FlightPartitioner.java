139a24dd506f50095c81df37a2f9278ff40accb7
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 01:29:40 +0300

    Scripted auto-commit on change (2020-10-10 01:29:40) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightPartitioner.java
+++ src/main/java/ru/Ivan/FlightPartitioner.java
@@ -6,6 +6,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class FlightPartitioner extends Partitioner<FlightWritableComparable, Text> {
     @Override
     public int getPartition(FlightWritableComparable flightWritableComparable, Text text, int i) {
-        return 0;
+        return (key.getID.hashCode() & Integer.MAX_VALUE) % i;
     }
 }
