517f740bb8066c032f4118c8019256a010e9d23d
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 01:41:00 +0300

    Scripted auto-commit on change (2020-10-10 01:41:00) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightReduce.java
+++ src/main/java/ru/Ivan/FlightReduce.java
@@ -3,5 +3,6 @@ package ru.Ivan;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class FlightReduce extends Reducer<FlightWritableComparable, Text, > {
+public class FlightReduce extends Reducer<FlightWritableComparable, Text, Text, Text> {
+    
 }
