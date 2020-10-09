da4e0a947e94fe8e93142bc7640b0fe1d252c3d7
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 01:26:12 +0300

    Scripted auto-commit on change (2020-10-10 01:26:12) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightNameMapper.java
+++ src/main/java/ru/Ivan/FlightNameMapper.java
@@ -8,9 +8,16 @@ import java.io.IOException;
 
 public class FlightNameMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
 
+    private static final String DELIMITER = ",";
+    private static final int FORNAME = 0;
+    private static final int DESTAEROPORTID = 0;
+    private static final int NAME = 1;
 
     @Override
-    public void map(LongWritable key, Text value, Context context) throws IOException {
-
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] table = value.toString().split(DELIMITER);
+        int destAeroportID = Integer.parseInt(table[DESTAEROPORTID]);
+        FlightWritableComparable currentKey = new FlightWritableComparable(destAeroportID, FORNAME);
+        context.write(currentKey, new Text(table[NAME]));
     }
 }
