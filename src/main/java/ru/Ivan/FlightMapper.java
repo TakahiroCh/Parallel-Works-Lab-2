b89bbc07141457185eeec1ab798bebace58f37b2
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-09 23:57:58 +0300

    Scripted auto-commit on change (2020-10-09 23:57:58) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightMapper.java
+++ src/main/java/ru/Ivan/FlightMapper.java
@@ -8,8 +8,9 @@ import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, FlightWritable> {
 
+    private constant final String delimeter
     @Override
     public void main(LongWritable key, Text value, Context context) throws IOException {
-        
+
     }
 }
