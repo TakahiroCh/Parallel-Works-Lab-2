d6575d2093a814827d42208d0b6faeb98b6c90c4
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 01:46:18 +0300

    Scripted auto-commit on change (2020-10-10 01:46:18) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightReduce.java
+++ src/main/java/ru/Ivan/FlightReduce.java
@@ -8,13 +8,22 @@ import java.util.Iterator;
 
 public class FlightReduce extends Reducer<FlightWritableComparable, Text, Text, Text> {
 
+    private static final int TOCOUNT = 0;
+    private static final float ZERO = 0.0F;
+
     @Override
     protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text nameInfo = new Text(iter.next());
+        int count = TOCOUNT;
+        float min = ZERO;
+        float max = ZERO;
+        float min = ZERO;
+
+
         while(iter.hasNext()) {
-            
+
         }
 
     }
