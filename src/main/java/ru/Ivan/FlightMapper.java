c230ee59344d9650f5f4797db7c50aa21af6589f
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 00:05:31 +0300

    Scripted auto-commit on change (2020-10-10 00:05:31) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightMapper.java
+++ src/main/java/ru/Ivan/FlightMapper.java
@@ -9,14 +9,19 @@ import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, FlightWritable> {
 
     private static final String DELIMITER = ",";
+    private static final int DESTAEROPORTID = 14;
+    private static final int ARRDELAY = 17;
+    private static final int CANCELLED = 19;
+    private static final int AIRTIME = 20;
 
     @Override
     public void main(LongWritable key, Text value, Context context) throws IOException {
         String[] table = value.toString().split(DELIMITER);
-        int destAeroportID = Integer.parseInt(table[14]);
-        float arrDelay = Float.parseFloat(table[17]);
-        float cancelled = Float.parseFloat(table[19]);
-        float airTime = Float.parseFloat(table[20]);
+        int destAeroportID = Integer.parseInt(table[DESTAEROPORTID]);
+        float arrDelay = Float.parseFloat(table[ARRDELAY]);
+        float cancelled = Float.parseFloat(table[CANCELLED]);
+        float airTime = Float.parseFloat(table[AIRTIME]);
+        
 
     }
 }
