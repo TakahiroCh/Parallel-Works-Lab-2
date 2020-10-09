c9a23567a08e99651c1b2eccf4cce702fac81b76
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 02:04:06 +0300

    Scripted auto-commit on change (2020-10-10 02:04:06) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightJob.java
+++ src/main/java/ru/Ivan/FlightJob.java
@@ -3,6 +3,8 @@ package ru.Ivan;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FlightJob {
@@ -13,14 +15,12 @@ public class FlightJob {
             System.exit(-1);
         }
         Job job = Job.getInstance();
-
         job.setJarByClass(FlightJob.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightNameMapper.class);
 
-        job.setJobName("Join");
-
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
         job.setMapperClass(WordMapper.class);
 
