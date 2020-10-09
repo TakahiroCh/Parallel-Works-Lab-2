c5bb1ff26393c05cafec7c02a77b62f8e52b6953
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 02:02:21 +0300

    Scripted auto-commit on change (2020-10-10 02:02:21) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightJob.java
+++ src/main/java/ru/Ivan/FlightJob.java
@@ -1,4 +1,38 @@
 package ru.Ivan;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class FlightJob {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage: FlightJob <input path flight> <input path name> <output path>");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance();
+
+        job.setJarByClass(FlightJob.class);
+
+        job.setJobName("Join");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(WordMapper.class);
+
+        job.setReducerClass(WordReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setNumReduceTasks(2);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
 }
