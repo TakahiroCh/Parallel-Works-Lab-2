047c2b13baba760a699c528f78a02cb474efb281
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 03:24:07 +0300

    Scripted auto-commit on change (2020-10-10 03:24:07) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightMapper.java
+++ src/main/java/ru/Ivan/FlightMapper.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
 
     private static final String DELIMITER = ",";
+    private static final String NULLSTR = "";
     private static final int FORDELAY = 1;
     private static final float ZERO = 0.0F;
     private static final int DESTAEROPORTID = 14;
@@ -16,12 +17,20 @@ public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableCompa
     //private static final int CANCELLED = 19;
     //private static final int AIRTIME = 20;
 
+    private static float checkNull(String current) {
+        if (current.equals(NULLSTR)) {
+            return ZERO;
+        } else {
+            return Float.parseFloat(current);
+        }
+    }
+
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         if (key.get() > 0) {
             String[] table = value.toString().split(DELIMITER);
             int destAeroportID = Integer.parseInt(table[DESTAEROPORTID]);
-            float arrDelay = Float.parseFloat(table[ARRDELAY]);
+            float arrDelay = checkNull(table[ARRDELAY]);
             //float cancelled = Float.parseFloat(table[CANCELLED]);
             //float airTime = Float.parseFloat(table[AIRTIME]);
             if (arrDelay >= ZERO) {
