0129b2ad9eee50dd2e007826ecc1e054f9841663
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 04:22:30 +0300

    Scripted auto-commit on change (2020-10-10 04:22:29) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightMapper.java
+++ src/main/java/ru/Ivan/FlightMapper.java
@@ -33,7 +33,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableCompa
             float arrDelay = checkNull(table[ARRDELAY]);
             //float cancelled = Float.parseFloat(table[CANCELLED]);
             //float airTime = Float.parseFloat(table[AIRTIME]);
-            if (arrDelay >= ZERO) {
+            if (arrDelay > ZERO) {
                 FlightWritableComparable currentKey = new FlightWritableComparable(destAeroportID, FORDELAY);
                 context.write(currentKey, new Text(table[ARRDELAY]));
             }
