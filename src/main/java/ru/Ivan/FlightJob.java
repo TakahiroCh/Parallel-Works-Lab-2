6161071db62e266b7ca8f2a072749f3b0b61cd79
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-10 03:05:43 +0300

    Scripted auto-commit on change (2020-10-10 03:05:43) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightJob.java
+++ src/main/java/ru/Ivan/FlightJob.java
@@ -9,7 +9,6 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FlightJob {
-
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
             System.err.println("Usage: FlightJob <input path flight> <input path name> <output path>");
